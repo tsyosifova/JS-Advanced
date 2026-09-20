ba8986b74fc164f4875f5429a48218a48aedf244
Цветанка Йосифова <tsnaneva@gmail.com>
2026-09-20 19:59:08 +0300

    03.SumNumbers

---
+++ 04-JS-Advanced-DOM-Introduction-Lab/03.SumNumbers.js
@@ -0,0 +1,6 @@
+function calc() {
+    const num1 = document.getElementById('num1');
+    const num2 = document.getElementById('num2');
+
+    document.getElementById('sum').value = Number(num1.value) + Number(num2.value);
+}
