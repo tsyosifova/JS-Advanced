f31e7011c349629dfae608e26c5ec807c2ec43f3
Цветанка Йосифова <tsnaneva@gmail.com>
2026-09-20 20:56:32 +0300

    04.ShowMore

---
+++ 04-JS-Advanced-DOM-Introduction-Lab/04.ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    const text = document.getElementById('text');
+    const btn = document.getElementById('more');
+
+    text.style.display = 'inline';
+    btn.style.display = 'none';
+}
