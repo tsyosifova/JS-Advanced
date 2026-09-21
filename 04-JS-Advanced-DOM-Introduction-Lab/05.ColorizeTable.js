359148140f5d5177052eeccb514862a05d81d251
Цветанка Йосифова <tsnaneva@gmail.com>
2026-09-21 09:44:56 +0300

    05.ColorizeTable

---
+++ 04-JS-Advanced-DOM-Introduction-Lab/05.ColorizeTable.js
@@ -0,0 +1,10 @@
+function colorize() {
+    const rows = Array.from(document.getElementsByTagName('tr'));
+
+    for (let i = 1; i < rows.length; i += 2) {
+
+        let row = rows[i];
+
+        row.style.backgroundColor = 'Teal';
+    }
+}
